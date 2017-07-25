d9df0a6018010ad384f71deacb1dd575567bf5e3
Mitrx <d.alxv@yandex.com>
2017-07-25 17:01:37 +0300

    Implement get-issue function for authorization

--- src/authz_test/api/issues.clj
+++ src/authz_test/api/issues.clj
@@ -1,5 +1,8 @@
 (ns authz-test.api.issues
-  (:require [authz-test.db.issues :refer [issues]]))
+  (:require [authz-test.db.issues :as issues :refer [issues]]))
 
 (defn get-issues []
   issues)
+
+(defn get-issue [issue-id]
+  (issues/get-issue issue-id))
--- src/authz_test/db/issues.clj
+++ src/authz_test/db/issues.clj
@@ -4,11 +4,16 @@
               :title "My title"
               :author-id 1
               :responsible-id 3
+              :organization-id 1
               :lat 1.073
               :lng 2.002}
              {:issue-id 2
               :title "Second title"
               :author-id 4
+              :organization-id 2
               :responsible-id 3
               :lat 8.088
               :lng 7.072}])
+
+(defn get-issue [issue-id]
+  (first (filter #(= (:issue-id %) issue-id) issues)))
