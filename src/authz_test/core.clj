016a7e4cfaf2be9b912aacef39dfc98b469daa61
Mitrx <d.alxv@yandex.com>
2017-07-25 17:14:53 +0300

    Put authorization wrapper into routes

--- src/authz_test/core.clj
+++ src/authz_test/core.clj
@@ -1,27 +1,27 @@
 (ns authz-test.core
   (:gen-class)
-  (:require [compojure.api.sweet :refer [defapi context GET]]
-            [ring.util.http-response :as resp]
-            [org.httpkit.server :as httpkit]
-            [schema.core :as sc]
-            [mount.core :refer [defstate]]
-            [authz-test.api.users :as users]
+  (:require [authz-test.api.authorization :as authorization]
             [authz-test.api.issues :as issues]
-            [authz-test.api.organizations :as organizations]))
-
-(defn wrap-abac-authorization [handler]
-  (fn [req]
-    (let [print-req (clojure.pprint/pprint req)
-          uri (:uri req)
-          method (:request-method req)]
-       (handler req))))
+            [authz-test.api.organizations :as organizations]
+            [authz-test.api.users :as users]
+            [compojure.api.sweet :as sweet :refer [context defapi GET]]
+            [mount.core :refer [defstate]]
+            [org.httpkit.server :as httpkit]
+            [ring.util.http-response :as resp]
+            [schema.core :as sc]))
 
 (defapi app
   (context "/api" []
-           :middleware [wrap-abac-authorization]
+           :middleware [authorization/wrap-abac-authorization]
            users/users-routes
-           (GET "/issues" []
-                (resp/ok {:issues (issues/get-issues)}))
+           (context "/issues" []
+                    (GET "/" []
+                         (resp/ok {:issues (issues/get-issues)}))
+                    (GET "/:issue-id" [issue-id]
+                         :path-params [issue-id :- Long]
+                         :body-params [user-id :- Long
+                                       issue-id :- Long]
+                         (resp/ok {:issue (issues/get-issue issue-id)})))
            (GET "/organizations" []
                 (resp/ok {:organizations (organizations/get-organizations)}))))
 
