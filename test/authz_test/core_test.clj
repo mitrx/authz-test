06ce5a0f464fa8dfa2f10b4a2b29f4eb0835125f
Mitrx <d.alxv@yandex.com>
2017-07-25 17:15:36 +0300

    Write a test for get specific issue

--- test/authz_test/core_test.clj
+++ test/authz_test/core_test.clj
@@ -1,7 +1,20 @@
 (ns authz-test.core-test
-  (:require [clojure.test :refer :all]
-            [authz-test.core :refer :all]))
+  (:require [authz-test.core :refer :all]
+            [cheshire.core :as json]
+            [clojure.test :refer :all]
+            [ring.mock.request :as mock]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn get-json-from-response [resp]
+  (json/parse-string (slurp (:body resp)) true))
+
+(deftest general-case-authorization
+  (testing "General case authorization"
+    (let [req (-> (mock/request :get "/api/issues/1")
+                  (mock/content-type "application/json")
+                  (mock/body (json/generate-string {:user-id 4
+                                                    :issue-id 2})))
+          resp (app req)
+          body (get-json-from-response resp)]
+      (clojure.pprint/pprint body)
+      (clojure.pprint/pprint (:status resp))
+      (is (= 0 0)))))
