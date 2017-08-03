8d5c1ad3c690f28360aeee070d710ff7bd75d862
Mitrx <d.alxv@yandex.com>
2017-08-03 20:45:22 +0300

    Rewrite tests according to yada util functions

--- test/authz_test/core_test.clj
+++ test/authz_test/core_test.clj
@@ -1,20 +1,22 @@
 (ns authz-test.core-test
   (:require [authz-test.core :refer :all]
             [cheshire.core :as json]
+            [cheshire.parse :as cheshire]
             [clojure.test :refer :all]
-            [ring.mock.request :as mock]))
+            [ring.mock.request :as mock]
+            [yada.yada :as yada]))
 
 (defn get-json-from-response [resp]
-  (json/parse-string (slurp (:body resp)) true))
+  (json/parse-string (:body resp) true))
 
-; (deftest general-case-authorization
-;   (testing "General case authorization"
-;     (let [req (-> (mock/request :get "/api/issues/1")
-;                   (mock/content-type "application/json")
-;                   (mock/body (json/generate-string {:user-id 4
-;                                                     :issue-id 2})))
-;           resp (app req)
-;           body (get-json-from-response resp)]
-;       (clojure.pprint/pprint body)
-;       (clojure.pprint/pprint (:status resp))
-;       (is (= 0 0)))))
+(deftest general-case-authorization
+  (testing "General case authorization"
+    (let [; req (-> (mock/request :get "/api/issues/1")
+          ;         (mock/content-type "application/json")
+          ;         (mock/body (json/generate-string {:user-id 4
+          ;                                           :issue-id 2})))
+          resp (yada/response-for routes :get (str "/issues/" 1))
+          body (get-json-from-response resp)
+          ]
+      (clojure.pprint/pprint body)
+      (is (= 200 (:status resp))))))
