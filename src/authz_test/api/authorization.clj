14b02e5991b057afecf6a39f844391d9ea3fa305
Mitrx <d.alxv@yandex.com>
2017-08-03 20:47:07 +0300

    Implement abac authorization method

--- src/authz_test/api/authorization.clj
+++ src/authz_test/api/authorization.clj
@@ -4,43 +4,20 @@
             [cheshire.core :as json]
             [ring.util.http-response :as resp]))
 
-(defn get-json-from-response [resp]
-  (json/parse-string (slurp (:body resp)) true))
+(defn eval-predicate [predicate props]
+  (cond
+    (vector? predicate)
+    (case (first predicate)
+      :and (every? #(eval-predicate % props) (rest predicate))
+      :or (some #(eval-predicate % props) (rest predicate))
+      :not (not (eval-predicate (second predicate) props)))
+    (keyword? predicate)
+    (predicate props)))
 
-(defn get-attrs [req]
-  (print "hello"))
-
-(defn check-against-rules [attrs]
-  true)
-
-(defn authorized? [req]
-  (let [body (get-json-from-response req)
-        user-id (:user-id body)
-        user-organizations (organizations/get-user-organizations user-id)
-        issue-id (:issue-id body)
-        issue (issues/get-issue issue-id)
-        a (print issue)
-        issue-organization-id (:organization-id issue)
-        issue-organization (organizations/get-organization issue-organization-id)
-        author-id (:author-id issue)
-        responsible-id (:responsible-id issue)
-        organization-id (:organization-id issue)]
-
-    (clojure.pprint/pprint (str "OUTPUT: " user-id " " issue-id " " author-id " " responsible-id))
-    (or (= user-id author-id)
-        (= user-id responsible-id)
-        (some #(= (:organization-id %) issue-organization-id) user-organizations))))
-
-(defn wrap-abac-authorization [handler]
-  (fn [req]
-    (if (authorized? req)
-      (handler req)
-      (resp/unauthorized {:error {:message "not-authorized"}}))))
-
-;; (defmulti get-attrs (-> :entity))
-
-;; (defmethod get-attrs :users [params]
-;;   (str "hello"))
-
-;; (defmethod get-attrs :default [params]
-;;   (str "fock"))
+(defmethod yada.authorization/validate
+  :authz-test/abac
+  [ctx credentials authorization]
+  (let [predicate (-> authorization :predicate)
+        props (:properties ctx)]
+    (when (eval-predicate predicate props)
+      ctx)))
